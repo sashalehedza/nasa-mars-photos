055bc81ab7f13a8e528dceefe54e6b363c77e4dc
Sasha Lehedza <sashalehedza@gmail.com>
2021-12-12 19:28:59 +0200

    switch

--- src/service/api.js
+++ src/service/api.js
@@ -4,16 +4,13 @@ dotenv.config()
 
 // const photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=9AHfZKRZ4uMNIyjEGWIcAC73qt6B7bDBCsddYMOy`
 export const getPhotos = async () => {
-  //let api_key = process.env.REACT_APP_AsdffsdPI_KEY
-  let api_key
+  let api_key = process.env.REACT_APP_API_KEY
   let photosUrl
   if (api_key) {
     photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`
   } else if (!api_key) {
     api_key = 'DEMO_KEY'
     photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`
-  } else if (!api_key) {
-    photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=9AHfZKRZ4uMNIyjEGWIcAC73qt6B7bDBCsddYMOy`
   }
 
   console.log(api_key)
