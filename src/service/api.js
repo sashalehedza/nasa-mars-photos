0c00456d25504e046089ed3262147500fc6367fb
Sasha Lehedza <sashalehedza@gmail.com>
2021-12-12 17:22:37 +0200

    api visible

--- src/service/api.js
+++ src/service/api.js
@@ -4,14 +4,18 @@ dotenv.config()
 
 //const photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=9AHfZKRZ4uMNIyjEGWIcAC73qt6B7bDBCsddYMOy`
 export const getPhotos = async () => {
-  let api_key = process.env.REACT_APP_API_KEY
+  // let api_key = process.env.REACT_APP_API_KEY
+  let api_key
   let photosUrl
   if (api_key) {
     photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`
+    // } else if (!api_key) {
+    //   api_key = 'DEMO_KEY'
+    //   photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`
   } else if (!api_key) {
-    api_key = 'DEMO_KEY'
-    photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`
+    photosUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=9AHfZKRZ4uMNIyjEGWIcAC73qt6B7bDBCsddYMOy`
   }
-  console.log(api_key)
+
+  // console.log(api_key)
   return await axios.get(`${photosUrl}`)
 }
