30725190bb333b977abfd0fc87a90c02f685901c
Sasha Lehedza <sashalehedza@gmail.com>
2021-12-12 12:51:38 +0200

    mui select and single item

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,19 +1,44 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+
+// const PhotoItem = (props) => {
+//   return (
+//     <div>
+//       <div>Sol - {props.photo.sol}</div>
+//       <div>Rover - {props.photo.rover.name}</div>
+//       <div>Camera - {props.photo.camera.name}</div>
+//       <div>
+//         <img
+//           style={{ width: 200, height: 200 }}
+//           alt={props.photo.id}
+//           src={props.photo.img_src.toString().replace('http', 'https')}
+//         />
+//       </div>
+
+//       <hr />
+//     </div>
+//   )
+// }
+
 const PhotoItem = (props) => {
   return (
-    <div>
-      <div>Sol - {props.photo.sol}</div>
-      <div>Rover - {props.photo.rover.name}</div>
-      <div>Camera - {props.photo.camera.name}</div>
-      <div>
-        <img
-          style={{ width: 200, height: 200 }}
-          alt={props.photo.id}
-          src={props.photo.img_src.toString().replace('http', 'https')}
-        />
-      </div>
-
-      <hr />
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component='img'
+        alt={props.photo.id}
+        height='140'
+        image={props.photo.img_src.toString().replace('http', 'https')}
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+          Sol - {props.photo.sol}
+        </Typography>
+        <Typography>Rover - {props.photo.rover.name}</Typography>
+        <Typography>Camera - {props.photo.camera.name}</Typography>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/components/UI/MySelect.js
+++ src/components/UI/MySelect.js
@@ -1,15 +1,45 @@
 import React from 'react'
 
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+
+// const MySelect = ({ options, defaultValue, value, onChange }) => {
+//   return (
+//     <select value={value} onChange={(event) => onChange(event.target.value)}>
+//       <option value=''>{defaultValue}</option>
+//       {options.map((option) => (
+//         <option key={option.value} value={option.value}>
+//           {option.name}
+//         </option>
+//       ))}
+//     </select>
+//   )
+// }
+
 const MySelect = ({ options, defaultValue, value, onChange }) => {
   return (
-    <select value={value} onChange={(event) => onChange(event.target.value)}>
-      <option value=''>{defaultValue}</option>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.name}
-        </option>
-      ))}
-    </select>
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id='demo-simple-select-label'>{defaultValue}</InputLabel>
+        <Select
+          labelId='demo-simple-select-label'
+          id='demo-simple-select'
+          value={value}
+          label={defaultValue}
+          onChange={(event) => onChange(event.target.value)}
+        >
+          <MenuItem value=''>{defaultValue}</MenuItem>
+          {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
   )
 }
 
